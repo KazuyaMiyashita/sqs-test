5524b4aa3bb6fe49b6789fb309b5de625789c54e
KazuyaMiayshita <miy1924@gmail.com>
2020-11-18 08:47:15 +0900

    akka

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,4 +2,14 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.2"
+
+  val AkkaVersion = "2.6.8"
+  val AkkaHttpVersion = "10.2.0"
+  lazy val akka = Seq(
+    "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
+    "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+    "com.typesafe.akka" %% "akka-stream-typed" % AkkaVersion,
+    "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
+    "ch.qos.logback" % "logback-classic" % "1.2.3"
+  )
 }
